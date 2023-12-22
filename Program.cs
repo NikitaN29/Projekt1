5f439a95e7ed50e6383c027a9b2fb8922b2fa001
NikitaN29 <154610585+NikitaN29@users.noreply.github.com>
2023-12-22 18:39:29 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+
+namespace PasswordGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите длину пароля:");
+            int length = int.Parse(Console.ReadLine());
+
+            string password = GeneratePassword(length);
+
+            Console.WriteLine("Сгенерированный пароль: " + password);
+        }
+
+        static string GeneratePassword(int length)
+        {
+            string allowedChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+
+            Random random = new Random();
+            char[] password = new char[length];
+
+            for (int i = 0; i < length; i++)
+            {
+                password[i] = allowedChars[random.Next(allowedChars.Length)];
+            }
+
+            return new string(password);
+        }
+    }
+}
